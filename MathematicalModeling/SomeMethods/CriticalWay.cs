99b1fe3461e30c5f05a297c2a644366d0d0c2dcf
Никита Агеев <xisaby@gmail.com>
2023-03-28 16:25:47 +0300

    Не работает доделай как нибудь

---
+++ MathematicalModeling/SomeMethods/CriticalWay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalModeling.SomeMethods
+{
+    internal class CriticalWay
+    {
+
+        //Работа на события используя наработки из Комивояджора
+    }
+}
--- MathematicalModeling/SomeMethods/TravelingSalesmanProblem.cs
+++ MathematicalModeling/SomeMethods/TravelingSalesmanProblem.cs
@@ -20,57 +20,118 @@ namespace MathematicalModeling.SomeMethods
 
         }
 
+        //protected static int[,] InitRandNumb(int[,] Matrix)
+        //{
+        //    НАДО ЧТОБЫ НА СТОЛБЦЕ НЕ БЫЛО ПОВТОРЯЮЩИХСЯ ЗНАЧЕНИЙ
+        //    int rows = Matrix.GetUpperBound(0) + 1;
+        //    int columns = Matrix.Length / rows;
+        //    Random r = new Random();
+        //    int[] some = new int[rows];
+        //    for (int i = 0; i < rows; i++)
+        //    {
+        //        bool flag = true;
+        //        while (flag)
+        //        {
+        //            for (int j = 0; j < columns; j++)
+        //            {
+        //                if (i == j) { Matrix[i, j] = 0; }
+        //                else { Matrix[i, j] = r.Next(1, 6); }
+        //                for (int z = 0; z <= j; z++)
+        //                {
+        //                    if (Matrix[i, j] != 0 && Matrix[i, j] > 0)
+        //                    {
+        //                        continue;
+        //                    }
+        //                    else if (z != 0 && Matrix[i, z] == some[z] && j != z)
+        //                    {
+        //                        flag = true;
+        //                        Matrix[i, z] = r.Next(1, 6);
+        //                    }
+        //                    else if (flag == true) { flag = false; }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    Console.WriteLine("Была воспроизведена следующая матрица:");
+        //    ShowMatrix(Matrix);
+        //    return Matrix;
+
+        //    int count = r.Next(1, 6);
+        //    bool flag = false;
+        //    for (int k = 0; k < j; k++)
+        //    {
+        //        while (!flag)
+        //        {
+
+        //        }
+        //    }
+        //}
+
         protected static int[,] InitRandNumb(int[,] Matrix)
         {
-            //НАДО ЧТОБЫ НА СТОЛБЦЕ НЕ БЫЛО ПОВТОРЯЮЩИХСЯ ЗНАЧЕНИЙ
+            //It is always for this
             int rows = Matrix.GetUpperBound(0) + 1;
             int columns = Matrix.Length / rows;
             Random r = new Random();
-            int[] some = new int[rows];
+
+            //Надо сделать так чтобы если i==j было всегда 0, и не повторялись значения ряда 
             for (int i = 0; i < rows; i++)
             {
                 bool flag = true;
-                while(flag)
+
+                while (flag)
                 {
+                    int count = 0;
                     for (int j = 0; j < columns; j++)
                     {
+                        //Стадия заполнения
                         if (i == j) { Matrix[i, j] = 0; }
                         else { Matrix[i, j] = r.Next(1, 6); }
-                        for (int z = 0; z <= j; z++)
+                        count++;
+                        if (count >= 2)//Если заполенено больше двух значений
                         {
-                            if (Matrix[i, j] != 0 && Matrix[i, j] > 0)
+                            for (int z = 0; z <= j; z++)
                             {
-                                continue;
+                                if (Matrix[i, z] != 0)//Если не i == j
+                                {
+                                    if ( z != j) //Если взятые значения не одни и теже
+                                    {
+                                        if (Matrix[i,z] == Matrix[i,j])
+                                        {//Если значения равны
+                                            Matrix[i,z] = r.Next(1,6);
+                                        }
+                                        else { continue; }
+
+                                    }
+                                    else { continue; }
+                                }
+                                else { continue; }
+
+
                             }
-                            else if (z != 0 && Matrix[i, z] == some[z] && j != z)
-                            {
-                                flag = true;
-                                Matrix[i, z] = r.Next(1, 6);
-                            }
-                            else if (flag == true) { flag = false; }
+
                         }
+
+
                     }
+
                 }
+
+              
+
             }
-            Console.WriteLine("Была воспроизведена следующая матрица:");
-            ShowMatrix(Matrix);
+
+
+
+
             return Matrix;
-
-            //int count = r.Next(1, 6);
-            //bool flag = false;
-            //for (int k = 0; k < j; k++)
-            //{
-            //    while (!flag)
-            //    {
-
-            //    }
-            //}
         }
 
 
 
 
-        
+
+
 
 
 
