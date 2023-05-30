2decc10eeb8fb466ddc545de0695737aff747e28
Nanech <xisaby@gmail.com>
2023-05-30 11:19:22 +0300

    доделай джонсана

---
+++ MathematicalModeling/SomeMethods/Jhonson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalModeling.SomeMethods
+{
+    internal class Jhonson
+    {
+        public void StartThisMethod()
+        {
+            List<int> first = new List<int>() {2, 8, 4, 8, 6, 9 };
+            List<int> second = new List<int>() {3, 3, 6, 5, 8, 7 };
+
+            int minOfFirst = first.Min();
+            int minOfSecond = second.Min();
+
+            List<List<int>  > top = new List<List<int>>();
+            List <int[]> bottom = new List<int[]>();
+
+
+        }
+
+
+    }
+}
