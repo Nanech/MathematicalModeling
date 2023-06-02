65652da69a6576c8cdfc964090de3920baa13ac8
Nanech <xisaby@gmail.com>
2023-06-02 17:11:54 +0300

    сделал монте карло, помогите

--- MathematicalModeling/Models.cs
+++ MathematicalModeling/Models.cs
@@ -8,8 +8,6 @@ namespace MathematicalModeling
 {
     internal class Models
     {
-
-
         public static void HunterXVictim()
         {
             double startValueVictim = 100;
@@ -42,6 +40,213 @@ namespace MathematicalModeling
 
         }
 
+
+        public static void MonteCarlo()
+        {
+            int n = 100000, k = 0;
+            double x, y, S;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x =  rnd.NextDouble() * 2;
+                y =  rnd.NextDouble() * 2;
+                if ( Math.Pow(x-1, 2) + Math.Pow(y-1,2) <= 1)
+                {
+                    k++;
+                }
+            }
+            S = (4 * (double)k) / (double)n;
+
+            Console.WriteLine("Результат pi в ходе решении программы {0}", S); 
+            Console.WriteLine("Точное математическое pi {0}", Math.PI); 
+
+        }
+
+        public static void MonteCarloEtap4()
+        {
+            int n = 1000 *  10000, k = 0;
+            double x, y, S, a = 5, b = 8.5;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ( (x/3<y) && (y<x*(10-x)/5) )
+                {
+                    k++;
+                }
+            }
+            S = (a* b * (double)k)/ (double)n;
+
+            Console.WriteLine("Результат площади фигуры {0}", Math.Round(S, 2) );
+
+        }
+
+        public static void MonteCarloEtap5()
+        {
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 5, b = 8.5;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ((x / 3 < y) && (y < x * (10 - x) / 5))
+                {
+                    k++;
+                }
+            }
+            S = (a * b * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры {0}", Math.Round(S, 1) );
+        }
+
+
+  
+        public static void zad1()
+        {
+            // >7
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 1, b = 20;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b) - 5;
+                y = rnd.NextDouble() * (a);
+                if ( Math.Sin(x) > y  && y>0  )
+                {
+                    k++;
+                }
+            }
+            S =  (b * a * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры 1 {0}", S);
+        }
+
+        public static void zad2()
+        {
+            // >28.6
+
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 8, b = 7; // x - b, a - y
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ( y >= x/2 && y <= (x*(8-x))/2 )
+                {
+                    k++;
+                }
+            }
+            S = (b * a * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры 2 {0}", S);
+        }
+
+        public static void zad3()
+        {
+            // >48
+
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 6, b = 12; // x - b, a - y
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ( y<6 && y>Math.Pow(x-6,2)/6)
+                {
+                    k++;
+                }
+            }
+            S = (b * a * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры 3 {0}", S);
+        }
+
+
+        public static void zad4()
+        {
+            // >19 - 20
+
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 4, b = 10; // x - b, a - y
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ( y > x/5 && y<x*(12-x)/9 )
+                {
+                    k++;
+                }
+            }
+            S = (b * a * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры 4 {0}", S);
+        }
+
+        public static void zad5()
+        {
+            // >17.5, 18
+
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 4, b = 8; // x - b, a - y
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ( y > (8-x)/8 &&  y < x*(8-x)/4  )
+                {
+                    k++;
+                }
+            }
+            S = (b * a * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры 5 {0}", S);
+        }
+
+
+        public static void zad6()
+        {
+            // 1-1.5
+
+            int n = 1000 * 10000, k = 0;
+            double x, y, S, a = 2, b = 3; // x - b, a - y
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = rnd.NextDouble() * (b);
+                y = rnd.NextDouble() * (a);
+                if ( y < Math.Sin(x) && y > Math.Pow(x-2,2)/2 )
+                {
+                    k++;
+                }
+            }
+            S = (b * a * (double)k) / (double)n;
+
+            Console.WriteLine("Результат площади фигуры 6 {0}", S);
+        }
 
     }
 }
--- MathematicalModeling/Program.cs
+++ MathematicalModeling/Program.cs
@@ -28,7 +28,25 @@ namespace MathematicalModeling
 
             // Models
 
-            Models.HunterXVictim();
+            //Models.HunterXVictim();
+
+            //Models.MonteCarlo();
+
+            //Models.MonteCarloEtap4();
+
+            //Models.MonteCarloEtap5();
+
+            Models.zad1();
+
+            Models.zad2();
+
+            Models.zad3();
+
+            Models.zad4();
+
+            Models.zad5();
+
+            Models.zad6();
 
             Console.ReadKey();
         }
